e27f955799fd4c73468e7b122a5b1ed2387ee812
Bailey Camp <39453825+bazzadazza72@users.noreply.github.com>
2022-11-16 12:36:43 +1030

    Update files in repo

---
+++ includes/createDB.php
@@ -0,0 +1,34 @@
+<?php
+    // Declares the MySQL connection and the database name for later use
+	$conn = mysqli_connect("localhost:3306","root","");
+    $dbName = "acmeprods";
+
+	// Checks if the connection to MySQL was successful
+	if (mysqli_connect_errno())
+	{
+		echo "<p>Failed to connect to MySQL: " . mysqli_connect_error() . "</p>";
+	}
+	else
+	{
+		echo "<p>Connected to mysql</p>";
+        // Creates the database and tables
+		$query="DROP DATABASE IF EXISTS " . $dbName . ";";
+        $query.="CREATE DATABASE " . $dbName . ";";
+        $query.="USE " . $dbName . ";";
+        $query.="CREATE TABLE IF NOT EXISTS products(id int auto_increment not null primary key, prodName varchar(30) not null, prodFinish varchar (30) not null, prodUsage varchar (30) not null, prodCost float(8,2) not null);";
+
+		if (mysqli_multi_query($conn,$query)) 
+		{
+            // Shows a message to the user stating the products database was created because it didn't exist
+			echo "<p>The products database doesn't exist and was created.</p>";
+			do
+			{
+				mysqli_next_result($conn);	
+			} while (mysqli_more_results($conn));
+		}
+		else 
+		{
+			echo "<p>Error: " . mysqli_error($conn) ."</p>";
+		}
+	}
+?>
--- index.php
+++ index.php
@@ -1,44 +1,38 @@
 <!--
-DOOR LEVER INVENTORY ASSIGNMENT - Main
-Developed by Bailey Camp on 26/10/2022
+    DOOR LEVER INVENTORY ASSIGNMENT - Main
+    Developed by Bailey Camp on 26/10/2022
 -->
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Acme Ceramics - Login</title>
+        <title>Acme Door Lever - Add Product</title>
     </head>
     <body>
-        <?php
-		    $conn = @mysqli_connect("localhost:33063","root","");
-
-		    // Check connection
-		    if (mysqli_connect_errno()){
-			    echo "<p>Failed to connect to MySQL: " . mysqli_connect_error() . "</p>";
-		    } else {
-		    	echo "<p>Connected to DB</p>";
-		    	//connect to the database
-		    	if(!mysqli_select_db($conn,"test")){
-		    		echo "<p>Failed to select test db: " . mysqli_error($conn) . "</p>";
-		    	} else {
-		    		echo "<p>Connected to test DB</p>";
-		    		// Create table
-	    			$query = "DROP DATABASE IF EXISTS acmelever;";
-                    $query. = "CREATE DATABASE acmelever;";
-                    $query. = "USE acmelever;";
-                    $query. = "CREATE TABLE IF NOT EXISTS doorinv (id int not null auto_increment primary key, prodName varchar(30) not null, prodFinish varchar(30) not null, prodUsage varchar(30) not null, prodCost float(8,2) not null);";
-
-                    if (mysql_multi_query($conn,$query)) {
-                        echo "<p>Database and tables created successfully.</p>";
-                        do {
-                            mysqli_next_result($conn);
-                        } while (mysqli_more_results($conn));
-                    } else {
-                        echo "<p>There was an error: " . mysqli_error($conn) ."</p>";
-                    }
-	    		}
-	    	}
-	    ?>
+            <?php
+            $conn = @mysqli_connect("localhost:3306","root","");
+            $dbName = "acmeprods";
+    
+            // Checks if the MySQL connection was successful
+            if (mysqli_connect_errno())
+            {
+                echo "<p>Failed to connect to MySQL: " . mysqli_connect_error() . "</p>";
+            }
+            else
+            {
+                echo "<p>Connected to SQL server</p>";
+          
+                // Attempts to connect to the database, and if it fails, runs the code contained in 'createDB.php'
+                if(!mysqli_select_db($conn, $dbName))
+                {
+                    include("./includes/createDB.php");
+                }
+                else
+                {
+                    echo "<p>Connected to test DB</p>";
+                }
+            }
+        ?>
     </body>
 </html>
